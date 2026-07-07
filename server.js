9fec54c3106ff9b847776cfee0463aa35ce89d75
Sanchita Kamble <sanchita@example.com>
2026-07-07 21:57:04 +0530

    Remove MongoDB URI debug log

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./backend/config/db');
 
-console.log("Mongo URI Loaded:", process.env.MONGO_URI);
+
 
 // Connect to MongoDB
 connectDB();
